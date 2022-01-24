dd073bfa9ffc1dcd543739568f009fc92f4dbbfc
DhanushaDevinda <devindarajapaksha12@gmail.com>
2022-01-25 01:40:56 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter as Router , Route } from 'react-router-dom';
+
+import {Earnings} from "./component/Salary Calculator";
+
+
+function App() {
+  return (
+    <div className="">
+      <Earnings/>
+    </div>
+  );
+}
+
+export default App;
